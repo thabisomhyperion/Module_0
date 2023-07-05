fc74aaa5734d816b9a3f20fe42e6c3f8a9ed30c9
thabisomhyperion <72816102+thabisomhyperion@users.noreply.github.com>
2023-07-05 13:45:58 +0000

    syntax error

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+try{
+  
+  console.log("Tumelo","Hello World!")
+  
+}
+catch (error){
+
+  if (error instanceof SyntaxError)
+  {
+    console.error('Syntax Error: ', error.message)
+  }else
+  {
+    console.error('Error', error)
+  }
+}
 
